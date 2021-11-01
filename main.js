f2c9f3ef2b7c1f30cd8e03bd3382b99590782573
AranyakGamer <85429755+AranyakGamer@users.noreply.github.com>
2021-11-01 17:04:40 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function startup() 
+{ 
+    setTimeout(3);
+    document.getElementById("mom").innerHTMl = "i am Seema Singh" 
+    setTimeout(3)
+    document.getElementById("mom").innerHTMl = "I Like to am good at art"  
+   setTimeout(3)
+   document.getElementById("mom").innerHTMl = "Hi"
+
+}
